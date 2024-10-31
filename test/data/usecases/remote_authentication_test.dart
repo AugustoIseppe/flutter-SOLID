e89b7767ca4eff4a83facf48e9e7129fa26223cf
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2024-10-31 08:56:43 -0300

    move SUT creation to a setup method

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -9,25 +9,36 @@ class RemoteAuthentication {
   RemoteAuthentication({required this.httpClient, required this.url});
 
   Future<void> auth() async {
-    await httpClient.request(url: url);
+    await httpClient.request(
+      url: url,
+      method: 'post',
+    );
   }
 }
 
 abstract class HttpClient {
-  Future<void>? request({required String url}) {}
+  Future<void>? request({
+    required String url,
+    required String method,
+  }) {}
 }
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
-  // RemoteAuthentication test;
-  // sut -> System Under Test
-  // Queremos testar a regra de negócio do RemoteAuthentication, eu tenho que garantir que não vai misturar a regra de negócio com a implementação do HttpClient
-  test('Should call HttpClient with correct URL', () async {
-    final httpClient = HttpClientSpy();
-    final url = faker.internet.httpUrl();
-    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
-    await sut.auth();
-    verify(httpClient.request(url: url));
+  RemoteAuthentication? sut;
+  HttpClientSpy? httpClient;
+  String? url;
+  setUp(() {
+    httpClient = HttpClientSpy();
+    url = faker.internet.httpUrl();
+    sut = RemoteAuthentication(httpClient: httpClient!, url: url!);
+  });
+  test('Should call HttpClient with correct values', () async {
+    await sut!.auth();
+    verify(httpClient!.request(
+      url: url!,
+      method: 'post',
+    ));
   });
 }
