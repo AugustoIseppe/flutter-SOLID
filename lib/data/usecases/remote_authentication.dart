ca971a65f63dd9f161d59f8dac0e48571f911ba4
Augusto Iseppe <129420176+AugustoIseppe@users.noreply.github.com>
2024-10-31 22:15:20 -0300

    ensure RemoteAuthentication throws UnexpectedError on 400

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,6 +1,7 @@
-import 'package:project_circuit_flutter/data/http/http.dart';
-
+import '../../domain/helpers/domain_error.dart';
 import '../../domain/usecases/usecases.dart';
+
+import '../http/http.dart';
 
 class RemoteAuthentication {
   final HttpClient httpClient;
@@ -10,11 +11,15 @@ class RemoteAuthentication {
 
   Future<void> auth(AuthenticationParams params) async {
     final body = RemoteAuthenticationParams.fromDomain(params).toJson();
-    await httpClient.request(
-      url: url,
-      method: 'post',
-      body: body,
-    );
+    try {
+      await httpClient.request(
+        url: url,
+        method: 'post',
+        body: body,
+      );
+    } on HttpError {
+      throw DomainError.unexpected;
+    }
   }
 }
 
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,10 +1,12 @@
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
-import 'package:project_circuit_flutter/data/http/http.dart';
-import 'package:project_circuit_flutter/data/usecases/usecases.dart';
 import 'package:test/test.dart';
 
+import 'package:project_circuit_flutter/domain/helpers/helpers.dart';
 import 'package:project_circuit_flutter/domain/usecases/usecases.dart';
+
+import 'package:project_circuit_flutter/data/http/http.dart';
+import 'package:project_circuit_flutter/data/usecases/usecases.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
@@ -12,11 +14,16 @@ void main() {
   RemoteAuthentication? sut;
   HttpClientSpy? httpClient;
   String? url;
+
   setUp(() {
     httpClient = HttpClientSpy();
     url = faker.internet.httpUrl();
+    if (url == null) {
+      // Handle the case where url is null (e.g., throw an exception)
+    }
     sut = RemoteAuthentication(httpClient: httpClient!, url: url!);
   });
+
   test('Should call HttpClient with correct values', () async {
     final params = AuthenticationParams(
       email: faker.internet.email(),
@@ -27,5 +34,21 @@ void main() {
       'email': params.email,
       'password': params.secret,
     }));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', () async {
+    when(httpClient!.request(
+            url: anyNamed('url'),
+            method: anyNamed('method'),
+            body: anyNamed('body')))
+        .thenThrow(HttpError.badRequest);
+
+    final params = AuthenticationParams(
+      email: faker.internet.email(),
+      secret: faker.internet.password(),
+    );
+    final future = sut!.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
